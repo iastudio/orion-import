8ea4aa2aa039c7c4390691b13b7b43a5eb8f9221
Ivan Rudoy - Work <rudooy@gmail.com>
2014-05-08 23:14:27 +1100

    + GRUNT + GITIGNORE

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    autoprefixer: {
+        dist: {
+            files: {
+                'css/app.css': 'css/app.css'
+            }
+        }
+    },
+    jade: {
+      compile: {
+        files: [{
+          src: ['*.jade'],
+          expand: true,
+          ext: '.html',
+        }]
+      },
+      options: {
+        pretty: true,
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'i',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'i',
+        }]
+      }
+    },
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      compass: {
+        files: 'scss/**/*.scss',
+        tasks: ['compass', 'autoprefixer']
+      },
+      jade: {
+        files: ['*.jade'],
+        tasks: ['jade'],
+      },
+      imagemin: {
+        files: ['i/**/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask('build', ['compass']);
+  grunt.registerTask('default', ['build','watch','jade','imagemin']);
+}
